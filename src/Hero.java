fcd7f5ca240172401505b75f1b0f7411b8ea2c5a
sarraalqahtani <alqahtas@wfu.edu>
2022-01-26 17:17:50 -0500

    Initial commit

---
+++ src/Hero.java
@@ -0,0 +1,12 @@
+public class Hero {
+
+
+
+    public void debugging_hero() {
+        System.out.println(heroName);
+        System.out.println(hitPoints);
+        System.out.println(lightAttackDamage);
+        System.out.println(heavyAttackDamage);
+    }
+
+}
---
+++ src/Lab2.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class Lab2 {
+
+
+    public static void main(String[] args) {
+        //fix me
+    }
+}
